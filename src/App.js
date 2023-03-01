bad33f881036d9deadeae16931f14045f0af7188
EC2 Default User <ec2-user@ip-172-31-2-20.us-east-2.compute.internal>
2023-03-01 21:37:56 +0000

    style of array divs mostly done for setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
+
 import './App.css';
 
+
 function App() {
+  const [array, setArray] = useState([])
+  const [numValue, setNumValue] = useState("")
+  
+  let dataArray = []
+  let windowWidth = window.innerWidth - 200
+  let windowHeight = window.innerHeight - 220
+  let recommendedArrayLength = Math.floor(windowWidth / 8)
+
+  const changeData = (num) => {
+    for(let i = 0; i < num; i++){
+      dataArray.push(Math.floor(Math.random() * windowHeight + 10))
+    }
+    setArray(dataArray)
+  }
+  
+  const handleChange = (e) => {
+    setNumValue(e.target.value)
+    console.log(e.target.value)
+  }
+  
+  const handleSubmit = (e) => {
+    changeData(numValue)
+  }
+  
+  const handleClear = (e) => {
+    setArray([])
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Algorithm Visualizer</h1>
+      <h4>Your viewport width is: {windowWidth}</h4>
+      <h4>Recommended Array Length: {recommendedArrayLength}</h4>
+
+      <div className="number__form">
+      <Form>
+        <Form.Group>
+          <Form.Control
+            name="rowNum"
+            value={numValue}
+            style={{width: "5rem"}}
+            placeholder={recommendedArrayLength}
+            onChange={handleChange}
+            label='Number input' 
+            id='typeNumber' 
+            type='number'
+          />
+        </Form.Group>
+      </Form>
+      <Button onClick={handleSubmit}>Submit</Button>
+      <Button onClick={handleClear}>Clear</Button>
+      </div>
+      
+      <div className="array__container" style={{width: `${windowWidth}px`}}>
+        {
+          array?.map((el) => (
+            <div 
+              className="array__columns"
+              key={Math.random() * 50000} 
+              style={{height: `${el}px`}}>
+           </div>
+          ))
+        }
+      </div>
+      
     </div>
   );
 }
